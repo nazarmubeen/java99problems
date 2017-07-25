fd9cc62a51d4cda4b4874c138825f3118657d2b3
mnmubeen <fam.nazar@gmail.com>
2017-07-25 08:47:27 +0530

    Longest common subsequence version 2

--- src/dynamic_programming/LongestCommonSubsequence.java
+++ src/dynamic_programming/LongestCommonSubsequence.java
@@ -12,39 +12,26 @@ public class LongestCommonSubsequence {
 	static void buildMatrix(String a,String b)
 	{
 		System.out.println( a.length() +" "+b.length());
-		int[][] matrix=new int[b.length()][a.length()];
+		int[][] matrix=new int[b.length()+1][a.length()+1];
 
 		System.out.println(" ");
 		
-		for(int i=0;i<b.length();i++)
+		for(int i=0;i<=b.length();i++)
 		{
-			for(int j=0;j<a.length();j++)
+			for(int j=0;j<=a.length();j++)
 			{
-				
-				if(a.charAt(j)==b.charAt(i))
+				if(i==0 || j==0)
 				{
-				if(i>0 && j>0)
-				matrix[i][j]=matrix[i-1][j-1]+1;
-				else{
-					matrix[i][j]=1;
+					matrix[i][j]=0;
+					
 				}
 				
+				else if(a.charAt(j-1)==b.charAt(i-1))
+				{
+				matrix[i][j]=matrix[i-1][j-1]+1;
 				}
 				else{
-					if(i>0 && j>0)
-					{
 					matrix[i][j]=Math.max(matrix[i][j-1],matrix[i-1][j]);
-					}
-					else{
-						if(i>0)
-						{
-							matrix[i][j]=matrix[i-1][j];
-						}
-						else if(j>0)
-						{
-							matrix[i][j]=matrix[i][j-1];
-						}
-					}
 				}
 				
 			}
@@ -64,30 +51,27 @@ public class LongestCommonSubsequence {
 		
 		int i=matrix.length-1;
 		int j=matrix[0].length-1;
-		System.out.println(" i "+i + "j "+j);
+
+
+		
 		while(i>0 && j>0){
 	
-			System.out.println(" i "+i + "j "+j);
-			if((matrix[i][j]==matrix[i-1][j-1]+1))
+			int top=matrix[i-1][j];
+			int left=matrix[i][j-1];
+			
+			if((matrix[i][j]==top || matrix[i][j]==left))
 			{
-				System.out.println("true");
-				sequence.append(a.charAt(j));
+				int move=Math.max(top, left)==top?i--:j--;
+			}
+			else{		
+				sequence.append(a.charAt(j-1));
 				i--;
 				j--;
-			}
-			else if(matrix[i][j]==matrix[i-1][j] )
-			{
-				j--;
-				
-			}
-			else if(matrix[i][j]==matrix[i][j-1])
-			{
-			j--;
 			}
 		
 			
 		}
-		
+		System.out.println(" ");
 		System.out.println("sequence "+sequence);
 		return sequence;
 		
