6df36714ac9f4fbe1db1c082b76d0723f4ab1ce3
nazarmubeen <fam.nazar@gmail.com>
2019-04-22 22:03:14 +0530

    collection to map

---
+++ src/com/java/concepts/eight/CollectionToMap.java
@@ -0,0 +1,26 @@
+package com.java.concepts.eight;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CollectionToMap {
+
+	
+	public static void main(String[] args)
+	{
+		List<Book> books = Arrays.asList(
+			    new Book(1, "Modern Java Recipes", 49.99),
+			    new Book(2, "Java 8 in Action", 49.99),
+			    new Book(3, "Java SE8 for the Really Impatient", 39.99),
+			    new Book(4, "Functional Programming in Java", 27.64),
+			    new Book(5, "Making Java Groovy", 45.99),
+			    new Book(6, "Gradle Recipes for Android", 23.76)
+			);
+		Map<Integer,Book> map=books.stream().collect(Collectors.toMap(Book::getId, Function.identity()));
+		
+		System.out.println(" map is "+map);
+	}
+}
---
+++ src/com/java/concepts/eight/SortingMaps.java
@@ -0,0 +1,11 @@
+package com.java.concepts.eight;
+
+import java.nio.file.Files;
+
+public class SortingMaps {
+
+	public static void main(String[] args)
+	{
+
+	}
+}
