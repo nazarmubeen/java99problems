db243b9fb43dabda4819eb90d91b2a1b634fdabf
nazarmubeen <fam.nazar@gmail.com>
2019-07-11 20:00:06 +0530

    phase2

--- src/com/java/concepts/eight/SortingMaps.java
+++ src/com/java/concepts/eight/SortingMaps.java
@@ -1,11 +1,35 @@
 package com.java.concepts.eight;
 
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 
 public class SortingMaps {
 
-	public static void main(String[] args)
-	{
-
+	public static void main(String[] args){
+			List<Integer> list=new ArrayList<Integer>();
+		//Nazar n1=new Nazar();
+		//n1.number=12;
+		//Nazar n2=new Nazar();
+		//n2.number=3;
+		list.add(5);
+		list.add(8);
+	//	Collections.sort(list,(Nazar n1,Nazar n2)->n1.);
+	list.forEach(a-> {if(a%2==0)System.out.println(a);} );
+		/*System.err.println(x);;*/
+		//HashMap<Character,Integer> map=new HashMap<>();
+		//map.put('E', 1);
+		//System.out.println(map.containsKey('e'));
+/*	int a="sakjd";
+	System.out.println(a);*/
+		
+		Thread t=Thread.currentThread();
+		System.out.println(t);
+		
 	}
+	
 }
